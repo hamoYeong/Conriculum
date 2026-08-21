51bcca37c097298c097cc526adf245c7f0621326
yeong <rlatjdud6573@naver.com>
2026-08-21 14:51:19 +0900

    Initial Commit

---
+++ Conriculum/ConriculumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConriculumApp.swift
+//  Conriculum
+//
+//  Created by Seoyeong_Kim on 8/21/26.
+//
+
+import SwiftUI
+
+@main
+struct ConriculumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
